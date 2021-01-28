5179dac0fe25aed95217311838f47000bb4c5f10
myrdinaptaliesen <64499780+myrdinaptaliesen@users.noreply.github.com>
2021-01-28 11:00:04 +0100

    fixtures user products categories ok

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,15 +2,45 @@
 
 namespace App\DataFixtures;
 
-use Doctrine\Bundle\FixturesBundle\Fixture;
+use App\Entity\User;
+use App\Entity\Products;
+use App\Entity\Categories;
 use Doctrine\Persistence\ObjectManager;
+use Doctrine\Bundle\FixturesBundle\Fixture;
 
 class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        //Création automatique de 20 produits
+        for ($i = 0; $i < 20; $i++) {
+            $product = new Products();
+            $product->setNameProduct("Produit $i");
+            $product->setDescriptionProduct("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam finibus interdum ipsum, vel tincidunt neque efficitur vel. Donec sit amet dolor gravida est convallis bibendum. Ut at nisl nibh. Vivamus sodales turpis aliquet nisl vestibulum, nec suscipit augue euismod. Proin mattis massa at leo porta egestas tincidunt sed nulla. Donec pellentesque, turpis a tristique ultricies, dui risus mollis mauris, eu viverra ligula leo vitae dolor. Quisque eget semper sapien. Donec accumsan augue et felis porta eleifend. Nam blandit vehicula fringilla.");
+            $product->setPriceProduct(99);
+            $product->setStockProduct(5);
+            $manager->persist($product);
+        }
+
+        //Création automatique de 3 catégories
+        for ($i = 0; $i < 3; $i++) {
+        $category = new Categories;
+        $category->setNameCategory("Catégorie $i");
+        $manager->persist($category);
+        }
+
+        //Création d'un utilisateur
+        $user = new User;
+        $user->setEmail("ngicquel@arinfo.fr");
+        $user->setRoles(["ROLE_ADMIN"]);
+        $user->setPassword("admin");
+        $user->setFirstName("Nicolas");
+        $user->setLastName("Gicquel");
+        $user->setAdress("2 route de Notre Dame de Grace");
+        $user->setZipCode("44750");
+        $user->setCity("Quilly");
+        $manager->persist($user);
+
 
         $manager->flush();
     }
